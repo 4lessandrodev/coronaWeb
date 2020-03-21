0fce4093267271357576b8796c41ce95bf473c7b
Alessandro Menezes <alessandroadm@live.com>
2020-03-21 19:34:54 -0300

    atualizando

---
+++ inserts.sql
@@ -0,0 +1,9 @@
+INSERT INTO sintomas(descricao) VALUES('Coriza');
+INSERT INTO sintomas(descricao) VALUES('Nariz entupido');
+INSERT INTO sintomas(descricao) VALUES('Cansaço');
+INSERT INTO sintomas(descricao) VALUES('Tosse');
+INSERT INTO sintomas(descricao) VALUES('Dor de cabeça');
+INSERT INTO sintomas(descricao) VALUES('Dores no corpo');
+INSERT INTO sintomas(descricao) VALUES('Mal estar geral');
+INSERT INTO sintomas(descricao) VALUES('Dor de garganta');
+
