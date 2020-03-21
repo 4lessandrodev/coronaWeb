bf0084916ac89ea8682bd320b22da8bdda2436a1
João Lucas da Silva <joao.lsilva1198@gmail.com>
2020-03-21 18:04:04 -0300

    perfil de usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,14 +1,22 @@
 const usuarioModel = require('./../models/UsuarioModel');
+const perfilModel = require('./../models/PerfilModel')
 
 
 
 const cadastrarUsuario = (req, res, next) => {
   const usuario = new usuarioModel(req.body.telefone, req.body.senha);
-  usuario.salvarUsuario(usuario).then(resosta => {
-    res.send('usuario cadastrado');
+  usuario.salvarUsuario(usuario).then(resposta => {
+  cadastradoDePerfil(req, res, next, resposta.insertId)
   });
 };
 
+const cadastradoDePerfil = (req, res, next, id_usuario) => {
+  const perfil = new perfilModel(req.body.nome, req.body.email, req.body.idade, req.body.genero, id_usuario)
+  perfil.salvarPerfil(perfil).then(resposta => {
+    res.send(`usuario cadastrado`)
+  })
+
+  } 
 
 
 
---
+++ models/PerfilModel.js
@@ -0,0 +1,60 @@
+const conect = require('../config/BD_CONECT');
+
+class PerfilModel {
+  constructor (nome, email, idade, genero, id_usuario) {
+    this._nome = nome;
+    this._email = email;
+    this._idade = idade;
+    this._genero = genero;
+    this._id_usuario = id_usuario;
+  }
+  
+  get nome() {
+    return this._nome;
+  }
+  get email() {
+    return this._email;
+  }
+  get idade() {
+    return this._idade;
+  }
+  get genero(){
+      return this._genero;
+  }
+  get id_usuario(){
+      return this._id_usuario;
+  }
+  set nome(value){
+    this._nome = value
+  }
+  set email(value) {
+    this._email = value;
+  }
+  set idade(value) {
+    this._idade = value;
+  }
+  set genero(value){
+    this._genero = value;
+  }
+  set id_usuario(value){
+      this._id_usuario = value;
+  }
+  
+  salvarPerfil(perfil) {
+    return new Promise((resolve, reject) => {
+      conect.query(`INSERT INTO perfil(nome, email, idade, genero, id_usuario) VALUES(?,?,?,?,?)`, [perfil._nome, perfil._email, perfil._idade, perfil._genero, perfil._id_usuario], (err, result) => {
+        if (err) {
+          reject(err.message);
+        } else {
+          resolve(result);
+        }
+      });
+    });   
+  }
+}
+
+module.exports = PerfilModel;
+
+
+
+
